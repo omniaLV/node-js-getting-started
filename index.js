61024c092445730acceff268799f7f441e44b057
omniaLV <60088953+omniaLV@users.noreply.github.com>
2022-11-04 21:10:27 -0700

    e

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
 const express = require('express')
+const logMe = require('./logger')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-var fs = require("fs");
 
-  function logger(req, res, next) {
-    const user_ip =
-      req.headers["x-forwarded-for"] ||
-      req.connection.remoteAddress ||
-      req.socket.remoteAddress ||
-      (req.connection.socket ? req.connection.socket.remoteAddress : null); // ::1
-    const req_page = req.originalUrl; // /
-    const date = new Date(); //2019-10-10T18:35:21.967Z
-    console.log(date);
+const app = express();
 
-    const log = `------------------------------------------------------------------------------------------------------\n[${date}]:  |  Ip=${user_ip}  |  Req_page=${req_page}\n------------------------------------------------------------------------------------------------------\n`;
+app.use(logMe.logger)
 
-    fs.appendFile("logs.txt", log, err => {
-      if (err) throw err;
-    });
-
-    next();
-  }
-
-  logger();
-
-
-express()
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
